26c2d3ab3c37770b3f5a8c0ab44b799290b2699f
AstralRomance <andrdoncwork@mail.ru>
2019-04-24 12:05:48 +0300

    Add files via upload

--- ml_2.py
+++ ml_2.py
@@ -64,26 +64,61 @@ def glass():
     targets_encoded = le.fit_transform(targets)
     nbhrs = []
     scores = []
+    total_nbh = []
+    total_scores = []
     for i in ('euclidean', 'manhattan', 'chebyshev', 'minkowski'):
         for j in range(2, 30):
             neigh = nbh(n_neighbors=j, metric=i, n_jobs=-1)
             classifier = neigh.fit(features_encoded, targets_encoded)
-            print('Metric: {0}, n_neighbors: {1} - Score: {2}'.format(i, j ,classifier.score(features_encoded, targets_encoded)))
+            print('Metric: {0}, n_neighbors: {1} - Score: {2}'.format(i, j, classifier.score(features_encoded, targets_encoded)))
             nbhrs.append(j)
             scores.append(1-classifier.score(features_encoded, targets_encoded))
-        print('scores val:')
-        for k in scores:
-            print(k)
-        plt.figure()
-        plt.plot(nbhrs, scores)
-        plt.title(i)
+        total_nbh.append(nbhrs)
+        total_scores.append(scores)
         nbhrs = []
         scores = []
+    plt.figure()
+    lbl = ['euclidean', 'manhattan', 'chebyshev', 'minkowski']
+    for i, l in enumerate(lbl):
+        plt.plot(total_nbh[i-1], total_scores[i-1], label=l)
+    plt.legend()
     pred = neigh.predict([[1.516, 11.7, 1.01, 1.19, 72.59, 0.43, 11.44, 0.02, 0.1]])
     print('Predicted type: {0}'.format(pred))
 
 
+def svm():
+    df = pd.read_csv('svmdata4.txt', sep='\t')
+    train_data = df.drop('Colors', 1).values
+    train_res = df['Colors'].values
+    df = pd.read_csv('svmdata4test.txt', sep='\t')
+    user_data = df.drop('Colors', 1).values
+    user_res = df['Colors'].values
+    le = preprocessing.LabelEncoder()
+    train_res_encoded = le.fit_transform(train_res)
+    user_res_encoded = le.fit_transform(user_res)
+    total_scores_train = []
+    total_scores_test = []
+    total_nbh = []
+    for i in range(1, 40):
+        nbhr = nbh(n_neighbors=i, metric='euclidean', n_jobs=1)
+        classifier = nbhr.fit(train_data, train_res_encoded)
+        total_scores_train.append(classifier.score(train_data, train_res_encoded))
+        classifier = nbhr.fit(user_data, user_res_encoded)
+        total_scores_test.append(classifier.score(user_data, user_res_encoded))
+        total_nbh.append(i)
+    plt.figure()
+    plt_lst = [total_scores_test, total_scores_train]
+    for i, lbl in enumerate(['test', 'train']):
+        plt.plot(total_nbh, plt_lst[i], label=lbl)
+    plt.legend()
+
+
 tic_tac_toe()
+print('tic_tac_toe')
 spam()
+print('spam')
 glass()
+print('glass')
+svm()
+print('svm')
 plt.show()
